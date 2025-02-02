bb7258d56d4e76e3561da84c9d05c404a0fa5090
sdelatorre13 <sebastian.delatorre13@gmail.com>
2025-02-02 17:45:38 -0500

    printing report on book

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ def main():
     book_text = read_file(file_path)
     word_count = count_words(book_text)
     char_dict = count_characters(book_text.lower())
+    sorted_char = list_of_dicts(char_dict)
 
-    print(char_dict)
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{word_count} words found in the document\n")
+    for dicts in sorted_char:
+        char = dicts["character"]
+        if char.isalpha():
+            count = dicts["count"]
+            print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
 
 def read_file(path):
     with open(path) as f:
@@ -24,5 +32,17 @@ def count_characters(text):
             dict[char] = 1
 
     return dict
+
+def sort_key(dict):
+    return dict["count"]
+
+def list_of_dicts(dict):
+    list = []
+    for char in dict:
+        count = dict[char]
+        new_dict = {"character": char, "count": count}
+        list.append(new_dict)
+    list.sort(reverse=True, key=sort_key)
+    return list
 
 main()
