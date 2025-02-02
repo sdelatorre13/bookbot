238b773fe46eee197b247f42f0408ad604814dd0
sdelatorre13 <sebastian.delatorre13@gmail.com>
2025-02-02 16:56:58 -0500

    count words and characters

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 def main():
     file_path = "books/frankenstein.txt"
-    with open(file_path) as f:
-        file_contents = f.read()
     
-    print(file_contents)
+    book_text = read_file(file_path)
+    word_count = count_words(book_text)
+    char_dict = count_characters(book_text.lower())
+
+    print(char_dict)
+
+def read_file(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    dict = {}
+    for char in text:
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+
+    return dict
 
 main()
